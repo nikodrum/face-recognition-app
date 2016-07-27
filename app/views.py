6e93ed05879eab9681de96b4f536b1636eaf3f0d
Nikolai <prosto2307@gmail.com>
2016-07-27 22:47:20 +0300

    request should be with file

--- app/views.py
+++ app/views.py
@@ -8,15 +8,15 @@ from flask import render_template,request
 from app import app, models
 from PIL import Image
 
-@app.route('/')
-@app.route('/index/')
-def index():
-    return render_template('index.html')
-
 @app.route('/api/v0/recognize',methods = ['POST'])
 def recognition():
 
-    requestes_pic = request.data
-    responce_pic = models.Recognition(requestes_pic).recognize()
+    requested_pic = request.files['myImage']
 
+    print (type(requested_pic))
+    
+    if type(requested_pic) == bytes:
+        responce_pic = models.Recognition(requested_pic).recognize()
+    else:
+        responce_pic = str(type(requested_pic))
     return { 'img_data' : responce_pic }, 200
